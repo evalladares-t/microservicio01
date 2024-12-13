373139f7423e324c900d75b4fb22338f0e56090b
Edward Joseph Valladares Tavera <evalladarestav@gmail.com>
2024-12-13 04:05:44 -0500

    add properties create context beans for test

--- src/test/java/com/nttdata/bootcamp/microservicio01/Microservicio01ApplicationTests.java
+++ src/test/java/com/nttdata/bootcamp/microservicio01/Microservicio01ApplicationTests.java
@@ -2,8 +2,10 @@ package com.nttdata.bootcamp.microservicio01;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource("classpath:application-test.properties")
 class Microservicio01ApplicationTests {
 
   @Test
